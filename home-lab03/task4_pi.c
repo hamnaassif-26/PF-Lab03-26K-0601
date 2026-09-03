a62e8eb253aca3735649887dca55927b789b6fc3
hamnaassif-26 <hamnaassif@gmail.com>
2026-09-03 08:53:14 -0700

    Create task4_pi.c
    Add task 4

---
+++ home-lab03/task4_pi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	double Pi = 3.14159265358979;
+	printf("The value of Pi to 2 digits precision:  %.2f \n",Pi);
+	printf("The value of Pi to 4 digits precision:  %.4f \n",Pi);
+	printf("The value of Pi to 6 digits precision:  %.6f \n",Pi);
+	printf("The value of Pi to 10 digits precision: %.10f \n",Pi);
+	getch();
+	return 0;
+}
