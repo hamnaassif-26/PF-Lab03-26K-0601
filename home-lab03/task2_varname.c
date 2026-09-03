c559d5e84bf44d0ba895a128c3dd91a4a17637d3
hamnaassif-26 <hamnaassif@gmail.com>
2026-09-03 08:59:01 -0700

    PF Lab 03-6 home tasks

---
+++ home-lab03/task2_varname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int number = 26;
+	float ratio = 72.034;
+	char uni_name[] = "FAST NUCES KHI CAMPUS";
+	char letter = 'O';
+	double value = 123.4567;
+	long acc_num = 345778909;
+	
+	printf(" Numberr= %d | Ratio= %f | University Name= %s | Letter= %c | Value= %lf | Account Num= %ld \n",number,ratio,uni_name,letter,value,acc_num);
+	getch();
+	return 0;
+}
